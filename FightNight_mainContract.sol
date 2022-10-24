7817019e219565dc04de074be09e6f82dab68202
sajad <esky700@gmail.com>
2022-10-24 04:38:08 +0330

    Modify Contract v1

--- FightNight_mainContract.sol
+++ FightNight_mainContract.sol
@@ -1,8 +1,6 @@
-pragma solidity 0.8.3;
-
 // SPDX-License-Identifier: MIT
 
-
+pragma solidity 0.8.3;
 
 import "@openzeppelin/contracts/utils/Address.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
@@ -25,12 +23,10 @@ abstract contract IRewardDistributionRecipient is Ownable {
     }
 }
 
-// TODO: Make this use `GamesCore` and update `GamesCore` to support BNB.
-contract FightBetting is IRewardDistributionRecipient {
+contract FightNight_V1_BUSD_USDT is IRewardDistributionRecipient {
     using SafeMath for uint256;
     using SafeERC20 for IERC20;
 
-    // TODO: Remove after extending GamesCore.sol
     address public croupier;
 
     enum Fighter {
@@ -39,7 +35,9 @@ contract FightBetting is IRewardDistributionRecipient {
         Loomdart
     }
 
-    IERC20 public token;
+    // IERC20 public token;
+    IERC20 public BUSD;
+    IERC20 public USDT;
 
     string public constant desc = "CT Fight Night Dubai 2021: RookieXBT vs Loomdart";
 
@@ -53,22 +51,27 @@ contract FightBetting is IRewardDistributionRecipient {
 
     Fighter public winner;
 
-    mapping(address => uint256) public loomdartBNBBet;
-    mapping(address => uint256) public rookieBNBBet;
-    mapping(address => uint256) public loomdartXBLZDBet;
-    mapping(address => uint256) public rookieXBLZDBet;
+    // mapping(address => uint256) public LoomdartBUSDBet;
+    // mapping(address => uint256) public RookieBUSDBet;
+    // mapping(address => uint256) public LoomdartUSDTBet;
+    // mapping(address => uint256) public RookieUSDTBet;
 
-    uint256 public loomdartBNBPot;
-    uint256 public rookieBNBPot;
-    uint256 public loomdartXBLZDPot;
-    uint256 public rookieXBLZDPot;
+    mapping(address => uint256) public loomdartBUSDBet;
+    mapping(address => uint256) public rookieBUSDBet;
+    mapping(address => uint256) public loomdartUSDTBet;
+    mapping(address => uint256) public rookieUSDTBet;
 
-    event LoomdartBNBBet(address indexed user, uint256 amount);
-    event RookieBNBBet(address indexed user, uint256 amount);
-    event LoomdartXBLZDBet(address indexed user, uint256 amount);
-    event RookieXBLZDBet(address indexed user, uint256 amount);
+    uint256 public loomdartBUSDPot;
+    uint256 public rookieBUSDPot;
+    uint256 public loomdartUSDTPot;
+    uint256 public rookieUSDTPot;
 
-    event EarningsPaid(address indexed user, uint256 bnbEarnings, uint256 tokenEarnings);
+    event LoomdartBUSDBet(address indexed user, uint256 amount);
+    event RookieBUSDBet(address indexed user, uint256 amount);
+    event LoomdartUSDTBet(address indexed user, uint256 amount);
+    event RookieUSDTBet(address indexed user, uint256 amount);
+
+    event EarningsPaid(address indexed user, uint256 busdEarnings, uint256 usdtEarnings);
 
     modifier checkStatus() {
         require(!isFinal, "fight is decided");
@@ -78,39 +81,70 @@ contract FightBetting is IRewardDistributionRecipient {
         _;
     }
 
-    constructor(address _croupier, address _token) {
+    constructor(address _croupier, address _busd, address _usdt ) {
         croupier = _croupier;
-        token = IERC20(_token);
+        // token = IERC20(_token);
+        BUSD = IERC20(_busd);
+        USDT = IERC20(_usdt);
         rewardDistribution = msg.sender;
     }
 
-    function BNBBet(Fighter fighter) public payable checkStatus {
-        require(msg.value != 0, "no bnb sent");
+    // function BNBBet(Fighter fighter) public payable checkStatus {
+    //     require(msg.value != 0, "no bnb sent");
+    //     if (fighter == Fighter.Loomdart) {
+    //         LoomdartBUSDBet[msg.sender] += msg.value;
+    //         loomdartBUSDPot += msg.value;
+    //         emit LoomdartBUSDBet(msg.sender, msg.value);
+    //     } else if (fighter == Fighter.Rookie) {
+    //         RookieBUSDBet[msg.sender] += msg.value;
+    //         rookieBUSDPot += msg.value;
+    //         emit RookieBUSDBet(msg.sender, msg.value);
+    //     } else {
+    //         revert("LFG! Pick one already!");
+    //     }
+    // }
+
+    function BUSDBet(Fighter fighter, uint256 amount) public checkStatus {
+        require(amount != 0, "no token sent");
         if (fighter == Fighter.Loomdart) {
-            loomdartBNBBet[msg.sender] += msg.value;
-            loomdartBNBPot += msg.value;
-            emit LoomdartBNBBet(msg.sender, msg.value);
+            uint256 _before = BUSD.balanceOf(address(this));
+            BUSD.safeTransferFrom(msg.sender, address(this), amount);
+            uint256 _after = BUSD.balanceOf(address(this));
+            uint256 _amount = _after.sub(_before);
+            LoomdartBUSDBet[msg.sender] += _amount;
+            loomdartBUSDPot += _amount;
+            emit LoomdartBUSDBet(msg.sender, _amount);
         } else if (fighter == Fighter.Rookie) {
-            rookieBNBBet[msg.sender] += msg.value;
-            rookieBNBPot += msg.value;
-            emit RookieBNBBet(msg.sender, msg.value);
+            uint256 _before = BUSD.balanceOf(address(this));
+            BUSD.safeTransferFrom(msg.sender, address(this), amount);
+            uint256 _after = BUSD.balanceOf(address(this));
+            uint256 _amount = _after.sub(_before);
+            RookieBUSDBet[msg.sender] += _amount;
+            rookieBUSDPot += _amount;
+            emit RookieBUSDBet(msg.sender, _amount);
         } else {
             revert("LFG! Pick one already!");
         }
     }
 
-    function XBLZDBet(Fighter fighter, uint256 amount) public checkStatus {
+    function USDTBet(Fighter fighter, uint256 amount) public checkStatus {
         require(amount != 0, "no token sent");
         if (fighter == Fighter.Loomdart) {
-            token.safeTransferFrom(msg.sender, address(this), amount);
-            loomdartXBLZDBet[msg.sender] += amount;
-            loomdartXBLZDPot += amount;
-            emit LoomdartXBLZDBet(msg.sender, amount);
+            uint256 _before = USDT.balanceOf(address(this));
+            USDT.safeTransferFrom(msg.sender, address(this), amount);
+            uint256 _after = USDT.balanceOf(address(this));
+            uint256 _amount = _after.sub(_before);
+            LoomdartUSDTBet[msg.sender] += _amount;
+            loomdartUSDTPot += _amount;
+            emit LoomdartUSDTBet(msg.sender, _amount);
         } else if (fighter == Fighter.Rookie) {
-            token.safeTransferFrom(msg.sender, address(this), amount);
-            rookieXBLZDBet[msg.sender] += amount;
-            rookieXBLZDPot += amount;
-            emit RookieXBLZDBet(msg.sender, amount);
+            uint256 _before = USDT.balanceOf(address(this));
+            USDT.safeTransferFrom(msg.sender, address(this), amount);
+            uint256 _after = USDT.balanceOf(address(this));
+            uint256 _amount = _after.sub(_before);
+            RookieUSDTBet[msg.sender] += _amount;
+            rookieUSDTPot += _amount;
+            emit RookieUSDTBet(msg.sender, _amount);
         } else {
             revert("LFG! Pick one already!");
         }
@@ -137,35 +171,35 @@ contract FightBetting is IRewardDistributionRecipient {
         isFinal = true;
     }
 
-    function getFees() external onlyRewardDistribution returns (uint256 bnbFees, uint256 tokenFees) {
+    function getFees() external onlyRewardDistribution returns (uint256 busdFees, uint256 usdtFees) {
         require(!isFeesClaimed, "fees claimed");
         if (isFinal) {
             isFeesClaimed = true;
 
             if (winner == Fighter.Loomdart) {
-                bnbFees = rookieBNBPot.mul(1e18).div(1e20);
-                if (bnbFees != 0) {
-                    _safeTransfer(bnbFees, true);
+                busdFees = rookieBUSDPot.mul(1e18).div(1e20);
+                if (busdFees != 0) {
+                    _safeTransfer(busdFees, true);
                 }
-
-                tokenFees = rookieXBLZDPot.mul(1e18).div(1e20);
-                if (tokenFees != 0) {
-                    _safeTransfer(tokenFees, false);
+                usdtFees = rookieUSDTPot.mul(1e18).div(1e20);
+                if (usdtFees != 0) {
+                    _safeTransfer(usdtFees, false);
                 }
             } else if (winner == Fighter.Rookie) {
-                bnbFees = loomdartBNBPot.mul(1e18).div(1e20);
-                if (bnbFees != 0) {
-                    _safeTransfer(bnbFees, true);
+                busdFees = loomdartBUSDPot.mul(1e18).div(1e20);
+                if (busdFees != 0) {
+                    _safeTransfer(busdFees, true);
                 }
-
-                tokenFees = loomdartXBLZDPot.mul(1e18).div(1e20);
-                if (tokenFees != 0) {
-                    _safeTransfer(tokenFees, false);
+                usdtFees = loomdartUSDTPot.mul(1e18).div(1e20);
+                if (usdtFees != 0) {
+                    _safeTransfer(usdtFees, false);
                 }
             }
         }
     }
 
+
+    // double Check for tokens please :/
     function rescueFunds(address tokenAddress) external onlyRewardDistribution {
         if (tokenAddress == address(0)) {
             Address.sendValue(payable(msg.sender), address(this).balance);
@@ -174,86 +208,84 @@ contract FightBetting is IRewardDistributionRecipient {
         }
     }
 
-    function earned(address account) public view returns (uint256 bnbEarnings, uint256 tokenEarnings) {
+    function earned(address account) public view returns (uint256 busdEarnings, uint256 usdtEarnings) {
         if (isFinal) {
-            uint256 _loomdartBNBBet = loomdartBNBBet[account];
-            uint256 _rookieBNBBet = rookieBNBBet[account];
-            uint256 _loomdartXBLZDBet = loomdartXBLZDBet[account];
-            uint256 _rookieXBLZDBet = rookieXBLZDBet[account];
+            uint256 _LoomdartBUSDBet = LoomdartBUSDBet[account];
+            uint256 _RookieBUSDBet = RookieBUSDBet[account];
+            uint256 _LoomdartUSDTBet = LoomdartUSDTBet[account];
+            uint256 _RookieUSDTBet = RookieUSDTBet[account];
 
             uint256 winnings;
             uint256 fee;
 
-            if (winner == Fighter.Loomdart && _loomdartBNBBet != 0) {
-                winnings = rookieBNBPot.mul(_loomdartBNBBet).div(loomdartBNBPot);
+            if (winner == Fighter.Loomdart && _LoomdartBUSDBet != 0) {
+                winnings = rookieBUSDPot.mul(_LoomdartBUSDBet).div(loomdartBUSDPot);
                 fee = winnings.mul(1e18).div(1e20);
-                winnings -= fee;
-                bnbEarnings = _loomdartBNBBet.add(winnings);
-            } else if (winner == Fighter.Rookie && _rookieBNBBet != 0) {
-                winnings = loomdartBNBPot.mul(_rookieBNBBet).div(rookieBNBPot);
+                winnings = winnings.sub(fee);
+                busdEarnings = _LoomdartBUSDBet.add(winnings);
+            } else if (winner == Fighter.Rookie && _RookieBUSDBet != 0) {
+                winnings = loomdartBUSDPot.mul(_RookieBUSDBet).div(rookieBUSDPot);
                 fee = winnings.mul(1e18).div(1e20);
-                winnings -= fee;
-                bnbEarnings = _rookieBNBBet.add(winnings);
+                winnings = winnings.sub(fee);
+                busdEarnings = _RookieBUSDBet.add(winnings);
             }
 
-            if (winner == Fighter.Loomdart && _loomdartXBLZDBet != 0) {
-                winnings = rookieXBLZDPot.mul(_loomdartXBLZDBet).div(loomdartXBLZDPot);
+            if (winner == Fighter.Loomdart && _LoomdartUSDTBet != 0) {
+                winnings = rookieUSDTPot.mul(_LoomdartUSDTBet).div(loomdartUSDTPot);
                 fee = winnings.mul(1e18).div(1e20);
-                winnings -= fee;
-                tokenEarnings = _loomdartXBLZDBet.add(winnings);
-            } else if (winner == Fighter.Rookie && _rookieXBLZDBet != 0) {
-                winnings = loomdartXBLZDPot.mul(_rookieXBLZDBet).div(rookieXBLZDPot);
+                winnings = winnings.sub(fee);
+                usdtEarnings = _LoomdartUSDTBet.add(winnings);
+            } else if (winner == Fighter.Rookie && _RookieUSDTBet != 0) {
+                winnings = loomdartUSDTPot.mul(_RookieUSDTBet).div(rookieUSDTPot);
                 fee = winnings.mul(1e18).div(1e20);
-                winnings -= fee;
-                tokenEarnings = _rookieXBLZDBet.add(winnings);
+                winnings = winnings.sub(fee);
+                usdtEarnings = _RookieUSDTBet.add(winnings);
             }
         } else if (isCanceled) {
-            bnbEarnings = loomdartBNBBet[account] + rookieBNBBet[account];
-            tokenEarnings = loomdartXBLZDBet[account] + rookieXBLZDBet[account];
+            busdEarnings = LoomdartBUSDBet[account] + RookieBUSDBet[account];
+            usdtEarnings = LoomdartUSDTBet[account] + RookieUSDTBet[account];
         }
     }
 
     function getRewards() public {
         require(isFinal || isCanceled, "fight not decided");
 
-        (uint256 bnbEarnings, uint256 tokenEarnings) = earned(msg.sender);
-        if (bnbEarnings != 0) {
-            loomdartBNBBet[msg.sender] = 0;
-            rookieBNBBet[msg.sender] = 0;
-            _safeTransfer(bnbEarnings, true);
+        (uint256 busdEarnings, uint256 usdtEarnings) = earned(msg.sender);
+        if (busdEarnings != 0) {
+            LoomdartBUSDBet[msg.sender] = 0;
+            RookieBUSDBet[msg.sender] = 0;
+            _safeTransfer(busdEarnings, true);
         }
-        if (tokenEarnings != 0) {
-            loomdartXBLZDBet[msg.sender] = 0;
-            rookieXBLZDBet[msg.sender] = 0;
-            _safeTransfer(tokenEarnings, false);
+        if (usdtEarnings != 0) {
+            LoomdartUSDTBet[msg.sender] = 0;
+            RookieUSDTBet[msg.sender] = 0;
+            _safeTransfer(usdtEarnings, false);
         }
-        emit EarningsPaid(msg.sender, bnbEarnings, tokenEarnings);
+        emit EarningsPaid(msg.sender, busdEarnings, usdtEarnings);
     }
 
-    function _safeTransfer(uint256 amount, bool isBNB) internal {
-        uint256 balance;
-        if (isBNB) {
-            balance = address(this).balance;
-            if (amount > balance) {
-                amount = balance;
+    function _safeTransfer(uint256 _amount, bool isBUSD) internal {
+        uint256 _balance;
+        if (isBUSD) {
+            _balance = BUSD.balanceOf(address(this));
+            if (_amount > _balance) {
+                _amount = _balance;
             }
-            Address.sendValue(payable(msg.sender), amount);
+            BUSD.safeTransfer(msg.sender, _amount);
         } else {
-            balance = token.balanceOf(address(this));
-            if (amount > balance) {
-                amount = balance;
+            _balance = USDT.balanceOf(address(this));
+            if (_amount > _balance) {
+                _amount = _balance;
             }
-            token.safeTransfer(msg.sender, amount);
+            USDT.safeTransfer(msg.sender, _amount);
         }
     }
 
-    // TODO: Remove after extending GamesCore.sol
-    function setToken(address _token) external onlyOwner {
-        require(_token != address(0x0));
-        token = IERC20(_token);
-    }
+    // function setToken(address _token) external onlyOwner {
+    //     require(_token != address(0x0));
+    //     token = IERC20(_token);
+    // }
 
-    // TODO: Remove after extending GamesCore.sol
     function setCroupier(address _addr) public onlyOwner {
         croupier = _addr;
     }
