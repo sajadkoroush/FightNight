9b61c658fdfa395234d5f697b0cc458d761e32e1
sajad <esky700@gmail.com>
2022-10-28 17:00:00 +0330

    Add Web3

--- web3/connection_2.js
+++ web3/connection_2.js
@@ -1,3 +1,11 @@
+import {
+    Contract,
+    Provider
+} from 'ethers-multicall';
+import {
+    ethers
+} from 'ethers';
+
 // import * as ierc20 from './ABIs/ierc20.json' // assert {type: 'json'};
 
 // import WalletConnect from 'walletconnect/client';
@@ -11,15 +19,28 @@
 
 
 let ierc20;
-let tokenAddress = "0xEC5dCb5Dbf4B114C9d0F65BcCAb49EC54F6A0867";
-await Getierc20();
+// let tokenAddress = "0xEC5dCb5Dbf4B114C9d0F65BcCAb49EC54F6A0867";
 
 let provider;
 let accounts;
 let account;
 let signer;
 
-const MAXUINT = 2 **256 - 1;
+// testnet
+let USDTContractAddress = "0x337610d27c682E347C9cD60BD4b3b107C9d34dDd"
+let BUSDContractAddress = "0x337610d27c682E347C9cD60BD4b3b107C9d34dDd"
+
+// mainnet
+// let USDTContractAddress = ""
+// let BUSDContractAddress = ""
+
+
+
+const ContractAddressFight = "0xAAEe32a89704306127304DC00787aAC0b7f1aEf3";
+let FightNight_ABI;
+
+
+const MAXUINT = 2 ** 256 - 1;
 const MAX_UINT = etehrs.BigNumber.from("0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff")
 
 async function Getierc20() {
@@ -27,6 +48,17 @@ async function Getierc20() {
     const jsonRes = await response.json();
     ierc20 = jsonRes;
 }
+
+async function GetFightNightABI() {
+    const response = await fetch('/ABIs/FightNight_ABI.json');
+    const jsonRes = await response.json();
+    FightNight_ABI = jsonRes;
+}
+
+await Getierc20();
+await GetFightNightABI();
+
+
 
 
 // let web3Modal
@@ -183,13 +215,13 @@ async function GetChainInformation() {
     console.log(name, chainId)
 }
 
-async function ApproveFunction() {
+async function ApproveFunction(tokenAddress) {
     let provider = new ethers.providers.Web3Provider(window.ethereum, "any");
     const signer = await provider.getSigner();
     const abi = ierc20;
     const contract = new ethers.Contract(tokenAddress, abi, signer);
     const tx = await contract['approve'](
-        signer.getAddress(),
+        ContractAddressFight,
         MAXUINT
         // ethers.utils.parseEther("0.01")
         , {
@@ -202,12 +234,12 @@ async function ApproveFunction() {
         })
 }
 
-async function AllowanceFunction() {
+async function AllowanceFunction(tokenAddress) {
     let provider = new ethers.providers.Web3Provider(window.ethereum, "any");
     const signer = await provider.getSigner();
     const abi = ierc20;
     const contract = new ethers.Contract(tokenAddress, abi, signer);
-    let allowance = await contract.allowance(signer.getAddress(), signer.getAddress())
+    let allowance = await contract.allowance(signer.getAddress(), ContractAddressFight)
     allowance = ethers.utils.formatEther(allowance);
     console.log(allowance);
     return allowance;
@@ -222,7 +254,7 @@ async function BalanceETH() {
     return balance;
 }
 
-async function BalanceToken() {
+async function BalanceToken(tokenAddress) {
     let provider = new ethers.providers.Web3Provider(window.ethereum, "any");
     const signer = await provider.getSigner();
     const abi = ierc20;
@@ -231,11 +263,6 @@ async function BalanceToken() {
     balance = ethers.utils.formatEther(balance);
     console.log(balance);
     return balance;
-}
-
-
-async function AddNetwork() {
-    setChainId("0x89");
 }
 
 async function setChainId(chainId) {
@@ -258,11 +285,143 @@ async function setChainId(chainId) {
             break;
 
         case "1":
-
             ethereum.on('chainChanged', (chai1nId) => {
                 console.log("change")
             });
 
             break;
     }
+}
+
+async function AddNetwork() {
+    // For Test network
+    setChainId("0x89");
+}
+
+async function GetUSDTBalance() {
+    // testnet
+    await getBalance(USDTContractAddress);
+    // mainnet
+
+}
+
+async function GetBUSDBalance() {
+    // testnet
+    await getBalance(BUSDContractAddress);
+    // mainnet
+
+}
+
+async function GetAwllowanceUSDT() {
+    // testnet
+    AllowanceFunction(USDTContractAddress);
+    // mainnet
+
+}
+async function GetAwllowanceBUSD() {
+    // testnet
+    AllowanceFunction(BUSDContractAddress);
+    // mainnet
+
+}
+
+
+async function GetPlayerinformation() {
+    let provider = new ethers.providers.Web3Provider(window.ethereum, "any");
+    const signer = await provider.getSigner();
+    // const contract = new ethers.Contract(ContractAddressFight, FightNight_ABI, signer);
+    const contract = new Contract(ContractAddressFight, FightNight_ABI);
+
+    let Player_1_BUSD_Bet = contract.Player_1BUSDBet(signer.getAddress());
+    let Player_1_BUSD_Pot = contract.Player_1BUSDPot(signer.getAddress());
+    let Player_1_USDT_Bet = contract.Player_1USDTBet(signer.getAddress());
+    let Player_1_USDT_Pot = contract.Player_1USDTPot(signer.getAddress());
+    let Player_2_BUSD_Bet = contract.Player_2BUSDBet(signer.getAddress());
+    let Player_2_BUSD_Pot = contract.Player_2BUSDPot(signer.getAddress());
+    let Player_2_USDT_Bet = contract.Player_2USDTBet(signer.getAddress());
+    let Player_2_USDT_Pot = contract.Player_2USDTPot(signer.getAddress());
+
+    let InformationClientforPlayers = await provider.all(Player_1_BUSD_Bet, Player_1_BUSD_Pot,
+        Player_1_USDT_Bet, Player_1_USDT_Pot, Player_2_BUSD_Bet, Player_2_BUSD_Pot, Player_2_USDT_Bet, Player_2_USDT_Pot);
+
+    return InformationClientforPlayers;
+}
+
+async function HowMuchEarn() {
+    let provider = new ethers.provider.Web3Provider(window.ethereum, "any");
+    const signer = await provider.getSigner();
+    const contract = new ethers.Contract(ContractAddressFight, FightNight_ABI, signer);
+    let [busdEarn, usdtEarn] = await contract.earned(signer.getAddress());
+    console.log(busdEarn, usdtEarn);
+    return (busdEarn, usdtEarn);
+}
+
+async function isCanceledF() {
+    let provider = new ethers.provider.Web3Provider(window.ethereum, "any");
+    const signer = await provider.getSigner();
+    const contract = new ethers.Contract(ContractAddressFight, FightNight_ABI, signer);
+    let isCanceled = await contract.isCanceled();
+    console.log(isCanceled);
+    return (isCanceled);
+}
+
+
+async function isPausedF() {
+    let provider = new ethers.provider.Web3Provider(window.ethereum, "any");
+    const signer = await provider.getSigner();
+    const contract = new ethers.Contract(ContractAddressFight, FightNight_ABI, signer);
+    let isPaused = await contract.isPaused();
+    console.log(isPaused);
+    return (isPaused);
+}
+
+async function BUSDBetFunction(fighter, amount) {
+    let provider = new ethers.providers.Web3Provider(window.ethereum, "any");
+    const signer = await provider.getSigner();
+    const contract = new ethers.Contract(ContractAddressFight, FightNight_ABI, signer);
+    const tx = await contract['BUSDBet'](
+        fighter,
+        ethers.utils.parseEther(amount)
+        // ethers.utils.parseEther("0.01")      Use String for input please  :/
+        , {
+            from: signer.getAddress()
+        }
+    )
+    provider.sendTransaction(tx)
+        .then((txObj) => {
+            console.log('txHash', txObj.hash)
+        })
+}
+
+async function USDTBetFunction(fighter, amount) {
+    let provider = new ethers.providers.Web3Provider(window.ethereum, "any");
+    const signer = await provider.getSigner();
+    const contract = new ethers.Contract(ContractAddressFight, FightNight_ABI, signer);
+    const tx = await contract['USDTBet'](
+        fighter,
+        ethers.utils.parseEther(amount)
+        // ethers.utils.parseEther("0.01")      
+        , {
+            from: signer.getAddress()
+        }
+    )
+    provider.sendTransaction(tx)
+        .then((txObj) => {
+            console.log('txHash', txObj.hash)
+        })
+}
+
+async function FinalizeFightFunction(fighter) {
+    let provider = new ethers.providers.Web3Provider(window.ethereum, "any");
+    const signer = await provider.getSigner();
+    const contract = new ethers.Contract(ContractAddressFight, FightNight_ABI, signer);
+    const tx = await contract['finalizeFight'](
+        fighter, {
+            from: signer.getAddress()
+        }
+    )
+    provider.sendTransaction(tx)
+        .then((txObj) => {
+            console.log('txHash', txObj.hash)
+        })
 }
